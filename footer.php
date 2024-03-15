809f7d688de28b04e0b70831cd05f02dedbcf8de
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2024-03-15 18:29:48 +0700

    fix footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-<footer>
+<footer class="footer mt-auto py-3">
   <div class="container mt-5">
-    <div class="row">
+    <div class="row justify-content-center align-items-center">
       <div class="col-md-12">
         <p class="text-center">Manajemen Barang - Muhamad Galih H1D022052</p>
       </div>
